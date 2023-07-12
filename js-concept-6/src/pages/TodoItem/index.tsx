b883a0b7c246af634cce9c69aaecf485df77a9f3
Onurcan Sever <onsever@icloud.com>
2023-07-11 20:31:46 -0400

    refactor code

--- js-concept-6/src/pages/TodoItem/index.tsx
+++ js-concept-6/src/pages/TodoItem/index.tsx
@@ -2,17 +2,20 @@ import {JSX} from "react";
 import NavigationBar from "../../components/NavigationBar";
 import {TodoProps} from "../../App.tsx";
 import {useParams} from "react-router-dom";
+import { useLocalStorage} from "../../hooks/useLocalStorage.ts";
 
-export default function TodoItem({ title, description }: TodoProps): JSX.Element {
+export default function TodoItem(): JSX.Element {
     const { id } = useParams();
+    const [localData] = useLocalStorage<TodoProps[]>("todos");
+    const currentTodo = localData.find((todo: TodoProps) => todo.id === Number(id));
 
     return (
         <>
             <NavigationBar />
             <div className="container">
                 <h1>Todo Item {id}</h1>
-                <p>{title}</p>
-                <p>{description ? description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consequatur dicta eius eveniet excepturi, ipsam iusto, laboriosam molestiae nam nihil quas suscipit! Aperiam ea eos fuga impedit labore non possimus?"}</p>
+                <p>{currentTodo && currentTodo.title}</p>
+                <p>{currentTodo && (currentTodo.description ? currentTodo.description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consequatur dicta eius eveniet excepturi, ipsam iusto, laboriosam molestiae nam nihil quas suscipit! Aperiam ea eos fuga impedit labore non possimus?")}</p>
             </div>
         </>
     )
